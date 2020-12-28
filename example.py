2114acf1ec8856ce7e1c9e4b2fecf6f9084dbc98
Shang-Yu Chien <s1109119@gmail.com>
2020-12-28 23:05:54 +0800

    Add files via upload
    Type: fix
    Fix the problem about inputing the information type .

--- example.py
+++ example.py
@@ -8,7 +8,7 @@ f = open(filename,'a',encoding="utf-8")
 print("---------------------" , file=f)
 #輸出Google 日曆
 info_in("會議名稱",f)
-number= input("1. 縣長行程 2.邀請函 3.Line 訊息 4.函 登行程 5.開會通知, 6.致詞稿 : ")
+number= input("1. 縣長行程 2.邀請函 3.Line 訊息 4.函 登行程 5.函 6.開會通知, 7.致詞稿 : ")
 type1=["縣長行程","邀請函","LINE 訊息","函 登行程","函","開會通知","致詞稿"]
 location = input("輸入詳細地點: ")
 print("地點:",location,file=f)
@@ -18,9 +18,7 @@ info_add("聯絡人、頭銜及聯絡電話",f)
 info_in("業務科長",f,True)
 info_in("業務承辦",f,True)
 test,test1=1,1
-print("若時間為12:00-14:00")
-print("請輸入12001400")
-ttime=input("輸入開始結束時間: ")
+ttime=input("輸入開始結束時間，格式如 12001400 或1200: ")
 if (len(ttime)==8):
     print("活動時間 (%s:%s - %s:%s):" %(ttime[0:2],ttime[2:4],ttime[4:6],ttime[6:8]))
     change = input("不需要詳細流程?(y)或者任意鍵繼續")
@@ -34,12 +32,7 @@ if (len(ttime)==4):
     print("活動時間: %s:%s 開始" %(ttime[0:2],ttime[2:4]),file=f)
 test2=1
 while(test2==1 and test1==1 ):
-    print("範例：若時間為")
-    print("12:00 - 1300")
-    print("13:00 - 1400")
-    print("14:00 - 1500")
-    print("請輸入1200130014001500")
-    scheduletime=input("輸入流程時間: ")
+    scheduletime=input("輸入流程時間，格式如 1200140015001600 ，共有3個行程")
     length= len(scheduletime)
     numschedule=int(length/4-1)
     if (length%4==0):
@@ -50,7 +43,7 @@ while(test2==1 and test1==1 ):
 i=0
 schedule_box=[]
 while(test==1):
-    print("第%d個行程，共%d個",%(len(schedule_box)+1,numschedule))
+    print(len(schedule_box),numschedule)
     schedule=input("輸入流程內容")
     if (schedule != ""):
         schedule_box.append(schedule)
