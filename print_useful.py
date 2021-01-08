291aa25383f3a9016e859d7aecb32e9a4bad18d5
Shang-Yu Chien <s1109119@gmail.com>
2021-01-09 00:19:42 +0800

    feat: Add new module: time_table

--- print_useful.py
+++ print_useful.py
@@ -43,3 +43,46 @@ def info_add(s,f, ps=False):
                 else:
                     for i in range(0,length):
                         print(box[i],file=f)
+def time_table(test1,f, test2=1):
+    #test1: Determine if execute the function
+    #test2: Determine when to jump out the loop
+    while(test1==1  and test2==1 ):
+        print("輸入流程時間範例：")
+        print("1200140015001600")
+        #print("將輸入 12:00 - 14:00")
+        #print("      14:00 - 15:00")
+        #print("      15:00 - 16:00")
+        print("總共三個時間行程。")
+        scheduletime=input("請輸入流程時間: ")
+        length= len(scheduletime)
+        numschedule=int(length/4-1)
+        if (length%4==0 and length > 0):
+            test2=10
+            again=input("重來(r)或任意鍵繼續")
+            if (again=="r"):
+                test2=1
+        else:
+            print("錯誤請重新輸入")
+            test2=1
+    i=0
+    schedule_box=[]
+    while(test1==1  and test2==10 ):
+        print(len(schedule_box),numschedule)
+        schedule=input("輸入流程內容")
+        if (schedule != ""):
+            schedule_box.append(schedule)
+        if (len(schedule_box)==numschedule):
+            test2=100
+            again=input("重來(r)或任意鍵繼續")
+            if (again=="r"):
+                test2=10
+                schedule_box=[]
+                i=0
+    if (test1==1):
+        time=scheduletime
+        for j in range(0,numschedule):
+            i=j*4
+            print("%s:%s - %s:%s %s"
+                  %(time[i:i+2],time[i+2:i+4],
+                    time[i+4:i+6],time[i+6:i+8],schedule_box[j])
+                  ,file=f)
